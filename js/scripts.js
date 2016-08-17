b9522321750b2cf68405275776ae3741a38b08ff
Cornelius Range and Nick Hurtado <nick@>
2016-08-17 09:45:35 -0400

    create the directories and files for leapyear, check whether a year is divisivle by 4 using business logic

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Program should return true for years divisible by 4
+var leapyear = function(input) {
+  if (input % 4 === 0) //&& (input % 100 != 0) && ( input % 400 === 0)) {
+{
+    console.log("This is divisible by 4w");
+}
+  else {
+    console.log("this is not a leap year!");
+  }
+}
