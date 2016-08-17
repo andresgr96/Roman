e29987c46461b03e8f0337296f213316eb950876
Cornelius Range and Nick Hurtado <cornelius@>
2016-08-17 09:49:09 -0400

    check whether a number is not divisible by 100 and return true

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Program should return true for years divisible by 4
 var leapyear = function(input) {
-  if (input % 4 === 0) //&& (input % 100 != 0) && ( input % 400 === 0)) {
+  if ((input % 4 === 0) && (input % 100 != 0)) //&& ( input % 400 === 0)) {
 {
-    console.log("This is divisible by 4w");
+    console.log("This is divisible by 4 and not divisible by 100");
 }
   else {
     console.log("this is not a leap year!");
