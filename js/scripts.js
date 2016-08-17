4e18af514e87da1cb04972a2a33623db486abeb8
Cornelius Range and Nick Hurtado <cornelius@>
2016-08-17 10:02:58 -0400

    check to see whether year is not divisble by 100 OR divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 // Program should return true for years divisible by 4
-var leapyear = function(input) {
-  if ((input % 4 === 0) && (input % 100 != 0)) //&& ( input % 400 === 0)) {
-{
-    console.log("This is divisible by 4 and not divisible by 100");
+var leapyear = function(year) {
+  if  ((year % 4 === 0) && (year % 100 != 0) || (year % 400 === 0)) {
+    return true ;
 }
   else {
-    console.log("this is not a leap year!");
+    return false;
   }
 }
