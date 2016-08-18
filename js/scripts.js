61c688395ddb21ed0a43e74b4e575e5bff9d27ca
Andres Garcia and Nicolas Hurtado <andres@>
2016-08-18 15:20:36 -0400

    add code expanding the conversion up to 99

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var numerals1s = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-var numerals10s = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+var numerals1s = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var numerals10s = ["","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 var numerals100s = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
 var numerals1000s = ["M", "MM", "MMM"];
 var result = ""
@@ -22,11 +22,29 @@ $(document).ready(function() {
 
         for (var index = 0; index < number; index++) {
 
-          console.log(numerals1s[index]);
-          result = numerals1s[index];
+
+          result = numerals1s[number];
+          console.log(result);
         }
-        alert(result)
       }
+      else if ((number >= 10) && (number <= 99)) {
+        for (var index = 0; index < number; index++){
+
+          result = numerals10s[parseInt(numberSplit[0])] + numerals1s[parseInt(numberSplit[1])];
+          console.log(result);
+
+        }
+      }
+      else if ((number >= 100) && (number <= 999)) {
+        for (var index = 0; index < number; index++){
+
+          result = numerals100s[parseInt(numberSplit[0])] + numerals10s[parseInt(numberSplit[1])] + numerals1s[parseInt(numberSplit[2])];
+          console.log(result);
+        }
+      }
+
+
+
       // else if (number < 10) {
       //
       //   if (number === 1){
