79ebcaaad6e980913f966deed62ec75766c610ab
Andres Garcia and Nicolas Hurtado <andres@>
2016-08-18 15:37:20 -0400

    add code expaning the conversions up to all roman numericlas or 3999

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var numerals1s = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 var numerals10s = ["","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-var numerals100s = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-var numerals1000s = ["M", "MM", "MMM"];
+var numerals100s = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var numerals1000s = ["", "M", "MM", "MMM"];
 var result = ""
 
 
@@ -12,9 +12,9 @@ $(document).ready(function() {
     var number = $(".number").val();
     var numberSplit = number.split('');
 
-    if (isNaN(number)) {
+    if (number >= 4000) {
 
-      console.log("This is not a number");
+      console.log("The number must be less than 3999");
 
     }
 
@@ -42,41 +42,13 @@ $(document).ready(function() {
           console.log(result);
         }
       }
+      else {
+        for (var index = 0; index < number; index++) {
 
-
-
-      // else if (number < 10) {
-      //
-      //   if (number === 1){
-      //     answer = "I";
-      //   }
-      //   if (number === 2){
-      //     answer = "II";
-      //   }
-      //   if (number === 3){
-      //     answer = "III";
-      //   }
-      //   if (number === 4){
-      //     answer = "IV";
-      //   }
-      //   if (number === 5){
-      //     answer = "V";
-      //   }
-      //   if (number === 6){
-      //     answer = "VI";
-      //   }
-      //   if (number === 7){
-      //     answer = "VII";
-      //   }
-      //   if (number === 8){
-      //     answer = "VIII";
-      //   }
-      //   if (number === 9){
-      //     answer = "IX";
-      //   } console.log(answer);
-      //
-      //
-
+          result = numerals1000s[parseInt(numberSplit[0])] + numerals100s[parseInt(numberSplit[1])] + numerals10s[parseInt(numberSplit[2])] + numerals1s[parseInt(numberSplit[3])];
+          console.log(result);
+        }
+      }
 
 
   });
